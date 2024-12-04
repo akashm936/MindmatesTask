054a0cbc3504660e08d6a8f25291b2341733acd8
Akash Morwal <akashbhau101@gmail.com>
2024-12-04 18:32:17 +0530

    Initial commit

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactUs = () => {
   return (
-    <div className="text-center mt-12">
+    <div className="text-center mt-12" >
       <h1 className="text-3xl">Contact Us</h1>
       <p className="mt-4 text-lg">Developed By AKASH MORWAL</p>
       <p>Email : amorwal5@gmail.com</p>
