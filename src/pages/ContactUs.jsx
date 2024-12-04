7c0b0eadea6ec88ada8420064b9c4e88f1aed14d
Akash Morwal <akashbhau101@gmail.com>
2024-12-04 16:17:25 +0530

    Contact us Information added

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,7 +7,36 @@ const ContactUs = () => {
       <p className="mt-4 text-lg">Developed By AKASH MORWAL</p>
       <p>Email : amorwal5@gmail.com</p>
       <p>Phone No: +91 8956055703</p>
-      
+      <div className="flex justify-center space-x-4 mt-4">
+        
+        <a
+          href="https://github.com/akashm936"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300"
+        >
+          GitHub
+        </a>
+
+        
+        <a
+          href="https://www.linkedin.com/in/akash-morwal" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
+        >
+          LinkedIn
+        </a>
+
+        <a
+          href="https://akashm936.github.io/akashportfolio/" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition duration-300"
+        >
+          Portfolio
+        </a>
+      </div>
     </div>
   );
 };
